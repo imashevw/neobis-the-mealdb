23579cdd204277bb8cbb190135ce6789833c6620
bekzat <imashevbekzat77@gmail.com>
2023-11-27 21:06:04 +0300

    made up the header and main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div className='promo'>  
+            <div className="container">
+                <div className='promo__block'>
+                    <p className='subtitle'>Meal of the day</p>
+                    <h2 className='promo__title'>Fruit and Cream Cheese Breakfast Pastries</h2>
+                    <p className='promo__descr'>Breakfast | American</p>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+  }
+
+export default Main;
